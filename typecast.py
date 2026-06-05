dbc6f03092ab8e3bb106b309519da13964f66fde
Kaiden664 <kaidenarav@gmail.com>
2026-06-05 14:48:48 -0400

    Add files via upload

---
+++ typecast.py
@@ -0,0 +1,20 @@
+name = "Kaiden"
+age = 9
+is_student = true
+weight = 29.02
+print("name:",name)
+print("data type of name is":, type (name))
+print("age:",age)
+print("data type of age is":, type (age))
+print("weight:",weight)"
+print("data type of weight is":, type (weight))
+print("is_student:",is_student)
+print("data type of is_student is":,type (is_student))
+
+
+age = str(age)
+print(age)
+print("data type of age is",type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is",type(age))
